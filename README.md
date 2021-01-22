41adfe397c2ba8290f7edf31e7608634ecb907dc
thianda <thianda91@outlook.com>
2021-01-22 17:19:04 +0800

    Known bug: ip_from_last() get unexpected result

--- README.md
+++ README.md
@@ -1,21 +1,58 @@
 ## IP 备案防漏报核查工具
 
-本工具用于 IP 地址备案完整性自查。根据预设的全量 IP 地址范围。与已备案的 IP 全量地址进行对比，找出未备案的 IP 地址。
+本工具用于 IP 地址备案完整性自查。
 
 同时支持 IPv4，IPv6 地址。
 
 ## 使用方法
 
-- 运行小工具。
+### 功能1
 
-- 根据提示输入全量的 IP 地址所在 `txt` 文件。
+**根据预设的全量 IP 地址范围。与已有 IP 地址进行对比，找出遗漏的 IP 地址。**
+
+运行小工具。
+
+根据提示输入全量的 IP 地址所在 `txt` 文件。
 
   > 每行一个 IP（段）
 
-- 根据提示输入要核查的 IP 地址所在 `txt` 文件。
+根据提示输入已存在的 IP 地址所在 `txt` 文件。
 
   > 每行一个 IP（段）或者每行包含起始 IP + 终止 IP 
 
-- 根据提示选择输入的 IP 地址格式。
+输出对比结果到文件。
 
-- 对比出结果。
+### 功能2
+
+**IP地址格式转换，在 ip/mask 与 ip_start - ip_end 之间转换。（自动识别）**
+
+在命令行运行小工具：(假设小工具文件名为 `integriry.exe`)
+
+```sh
+integriry convert <单个ip或txt格式的文件名>
+```
+
+下面的例子都是正确的：
+
+```sh
+integriry convert 10.0.1.0/23
+integriry convert 192.168.0.0-192.168.1.159
+integriry convert 192.168.0.129-192.168.2.3
+integriry convert 2021:abcd::f00/120
+integriry convert 2021:abcd::f00-2021:abcd::8888:fff
+integriry convert input.txt
+```
+
+### 功能3
+
+**输入若干ip地址段，输出IP地址段合并后的结果**
+
+```sh
+integriry merge <txt格式的文件名>
+```
+
+例子：
+
+```sh
+integriry merge input.txt
+```
--- integriry.py
+++ integriry.py
@@ -11,7 +11,7 @@ from prompt_toolkit.formatted_text import FormattedText
 import sys
 
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = ''.join(chr(x) for x in [20110, 26174, 36798, 46, 38081, 23725])
 
 _RED = '#ff0066'
@@ -59,35 +59,38 @@ class ip_integriry(object):
         获取完整的 IP 范围
         使用 .readlines() 时，若文件结尾空行大于1，则会报错。
         使用 .read().strip().split('\n') 则不会。
+        已通过下面 get_input_ip() 实现
         '''
         # all_ip_file = config.get('common', 'all_ip_file')
         with open(all_ip_file, 'r') as f:
             all_ip_list = f.readlines()
+        # 不使用下面的单行函数返回结果，是为了在循环中使用try，若格式异常可跳过，不影响整体运行。
         # all_ip = IPSet([IP(x.strip(), make_net=True) for x in all_ip_list])
         all_ip = IPSet()
         for x in all_ip_list:
             try:
-                _i = IP(x.strip(),make_net=True)
+                _i = IP(x.strip(), make_net=True)
                 all_ip.add(_i)
             except Exception as err:
-                pft(str_red('忽略无效ip：{}，\n\t错误原因：{}'.format(x,err)))
+                pft(str_red('忽略无效ip：{}，\n\t错误原因：{}'.format(x, err)))
         return all_ip
 
     def get_input_ip(self, input_ip_file):
         '''
         获取输入的 IP
-        与 get_all_ip() 功能相同，运行原理不同而已
+        与 get_all_ip() 功能相同，运行原理不同而已，容错性更好
         '''
         with open(input_ip_file) as f:
-            input_ip_str = f.read().strip()
+            input_ip_str = f.read().strip().split('\n')
+        # 不使用下面的单行函数返回结果，是为了在循环中使用try，若格式异常可跳过，不影响整体运行。
         #input_ip = IPSet([IP(x.strip(), make_net=True) for x in input_ip_str.split('\n')])
         input_ip = IPSet()
-        for x in input_ip_str.split('\n'):
+        for x in input_ip_str:
             try:
-                _i = IP(x.strip(),make_net=True)
+                _i = IP(x.strip(), make_net=True)
                 input_ip.add(_i)
             except Exception as err:
-                pft(str_red('忽略无效ip：{}，\n\t错误原因：{}'.format(x,err)))
+                pft(str_red('忽略无效ip：{}，\n\t错误原因：{}'.format(x, err)))
         return input_ip
 
     def compare(self, all_ip=IPSet(), input_ip=IPSet()):
@@ -96,7 +99,7 @@ class ip_integriry(object):
         self.result = '\n'.join([x.strCompressed()
                                  for x in (all_ip - input_ip)])
         t1 = (datetime.now() - t0).total_seconds()
-        pft(str_blue('运行用时：{:4.2f} s'.format(t1)))
+        pft(str_blue_green('运行用时：', '{:4.2f} s'.format(t1)))
 
     def incomplete(self):
         if sys.argv[-1] == '--cli':
@@ -143,12 +146,75 @@ class ip_integriry(object):
             self.complete()
 
 
+def ip_to_last(_ip: str):
+    '''ip/mask 转换为 ip_start-ip_end'''
+    return '{}\t{}'.format(IP(_ip)[0].strCompressed(), IP(_ip)[-1].strCompressed())
+
+
+def ip_from_last(_ip: str):
+    '''ip_start-ip_end 转换为 ip/mask'''
+    _start, _end = '', ''
+    try:
+        if '\t' in _ip:
+            _start, _end = _ip.split('\t')
+        if '-' in _ip:
+            _start, _end = _ip.split('-')
+    except:
+        pft(str_red('IP格式不对，请检查。'))
+        exit('>>运行结束。')
+
+    def ip_len(a, b):
+        return IP(b).int()-IP(a).int()+1
+    ip_len_bin_str = '{:b}'.format(ip_len(_start, _end))
+    nn = []
+    for x in range(len(ip_len_bin_str)):
+        if ip_len_bin_str[x] == "1":
+            nn.append(x)
+    _ver_len = 129 if _ip.find(':') != -1 else 33
+    ll = [_ver_len-len(ip_len_bin_str)+x for x in nn]
+    _res = []
+    for x in ll:
+        _x = IP(_start).make_net(x)
+        _res.append(_x.strNormal())
+        _start = IP(_x[-1].int()+1).strNormal()
+    return _res
+
+
+def ip_convert(_ip: str):
+    '''IP 地址格式转换，自动判断，在 ip/mask 与 ip_start-ip_end 之间转换'''
+
+    res = []
+    _ip_list = _ip.strip().split('\n')
+    try:
+        ip_list = [IP(x.strip()) for x in _ip_list]
+        # 每行一个 IP，则转换成 ip_start-ip_end
+        for x in ip_list:
+            res.append(ip_to_last(x))
+    except:
+        for x in _ip_list:
+            res.extend(ip_from_last(x.strip()))
+    return '\n'.join(res)
+
+
+def ip_merge(file_name: str):
+    '''IP 地址合并计算'''
+    ...
+
+
 if __name__ == '__main__':
-    # try:
-    result = ip_integriry()
-    result.main()
-    # except Exception as err:
+    args = sys.argv
+    if len(args) == 1:
+        # try:
+        result = ip_integriry()
+        result.main()
+        # except Exception as err:
         # print('出错啦。错误信息：')
         # print(err)
+    elif args[1] == 'convert':
+        print(ip_convert(args[2]))
+    elif args[1] == 'merge':
+        print(ip_merge(args[2]))
+    else:
+        print('输入有误，请查看使用说明！')
     print('\n>>>>>>>>>>\n按任意键退出...\n')
     input()
